f307975adc71f9f366569a6799d88c9193c95874
Victor Presumido <victor.presumido@gmail.com>
2018-11-26 19:07:13 +0100

    📈 Fix powerline

--- emacs/lisp/init-platform.el
+++ emacs/lisp/init-platform.el
@@ -1,22 +1,15 @@
 (when (eq system-type 'darwin)
 
       ;; default Latin font (e.g. Consolas)
-      (set-face-attribute 'default nil :family "Hack")
+      (set-face-attribute 'default nil :family "DejaVu Sans Mono for Powerline")
 
       ;; default font size (point * 10)
       ;;
       ;; WARNING!  Depending on the default font,
       ;; if the size is not supported very well, the frame will be clipped
-      ;; so that the beginning of the buffer may not be visible correctly. 
-      (set-face-attribute 'default nil :height 165)
+      ;; so that the beginning of the buffer may not be visible correctly.
+      (set-face-attribute 'default nil :height 140)
 
-      ;; use specific font for Korean charset.
-      ;; if you want to use different font size for specific charset,
-      ;; add :size POINT-SIZE in the font-spec.
-      (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
-
-      ;; you may want to add different for other charset in this way.
       )
-
 
 (provide 'init-platform)
--- emacs/lisp/init-powerline.el
+++ emacs/lisp/init-powerline.el
@@ -1,6 +1,28 @@
-(use-package powerline-evil
-    :ensure t
-    :config
-    (powerline-evil-vim-color-theme))
+(use-package spaceline
+ :ensure t
+ :config
 
+ (use-package spaceline-config
+   :ensure spaceline
+   :config
+   (spaceline-spacemacs-theme)
+   (spaceline-helm-mode)
+   (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+   (setq powerline-default-separator 'arrow)
+   (setq ns-use-srgb-colorspace nil)
+   (setq powerline-height 20)
+   (spaceline-toggle-buffer-size-off)
+   (spaceline-compile)
+   )
+ (use-package diminish :ensure t
+   :config
+   (diminish 'undo-tree-mode)
+   (diminish 'which-key-mode)
+   (diminish 'flyspell-mode)
+   (diminish 'ivy-mode)
+   (diminish 'flycheck-mode)
+   (diminish 'eldoc-mode)
+   (diminish 'overwrite-mode)
+));; fix powerline separators
+(setq powerline-default-separator 'utf-8)
 (provide 'init-powerline)
