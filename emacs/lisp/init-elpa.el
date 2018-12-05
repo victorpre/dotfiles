4b47b29846d609de5d26fe301e5d15381ce1e804
Victor Presumido <victor.presumido@gmail.com>
2018-12-05 13:50:38 +0100

    Small fixes

--- emacs/lisp/init-elpa.el
+++ emacs/lisp/init-elpa.el
@@ -15,7 +15,7 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
 (provide 'init-elpa)
