9d8b98d386e0b32d89162edd71d07e495f8a4552
Victor Presumido <victor.presumido@gmail.com>
2019-01-18 10:58:26 +0100

    📈 Presentation in org-mode

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -2,4 +2,21 @@
 (global-set-key "\C-ca" 'org-agenda)
 
 
+(use-package ox-twbs
+            :ensure t)
+
+(use-package hide-mode-line
+             :ensure t)
+
+;; Org-present
+(require 'org-present)
+(add-hook 'org-present-mode-hook #'hide-mode-line-mode)
+(add-hook 'org-present-mode-hook  (lambda () (linum-mode 0)))
+(add-hook 'org-present-mode-hook  (lambda () (evil-mode 0)))
+
+
+;; Zooming
+(global-set-key (kbd "C-+") #'text-scale-increase)
+(global-set-key (kbd "C-\-") #'text-scale-decrease)
+
 (provide 'init-org)
