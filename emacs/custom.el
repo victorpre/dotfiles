a75fadb606dbbd12c9817d3c6397ba6ce5641f81
Victor Presumido <victor.presumido@gmail.com>
2019-02-26 18:24:23 +0100

    🙈 Leftovers

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,12 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(org-agenda-files (quote ("~/Code/work.org" "~/Code/new2.org")))
  '(package-selected-packages
    (quote
-    (powerline-evil evil-indent-textobject evil-surround evil-leader evil use-package))))
+    (rhtml-mode yard-mode robe powerline moe-theme spaceline-all-the-icons exec-path-from-shell flycheck fancy-battery spaceline spaceline-config powerline-evil evil-indent-textobject evil-surround evil-leader evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(telephone-line-evil-emacs ((t (:inherit telephone-line-evil :background "#6c71c4"))))
+ '(telephone-line-evil-insert ((t (:inherit telephone-line-evil :background "#268bd2"))))
+ '(telephone-line-evil-motion ((t (:inherit telephone-line-evil :background "DarkSlateBlue"))))
+ '(telephone-line-evil-normal ((t (:inherit telephone-line-evil :background "#d33682"))))
+ '(telephone-line-evil-operator ((t (:inherit telephone-line-evil :background "VioletRed"))))
+ '(telephone-line-evil-replace ((t (:inherit telephone-line-evil :background "#dc322f"))))
+ '(telephone-line-evil-visual ((t (:inherit telephone-line-evil :background "#cb4b16")))))
